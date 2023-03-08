f52ee5aa30c79b6257dbe360c4d4807517addfc1
Jonathan Sarmiento <jhonn.photo@gmail.com>
2023-03-08 07:32:14 -0500

    feat/generating response with custom status implemented

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,16 +1,17 @@
-import { Controller } from '@nestjs/common';
-import { Body, Get, Param, Post } from '@nestjs/common/decorators';
+import { Controller, HttpStatus } from '@nestjs/common';
+import { Body, Get, HttpCode, Param, Post, Res } from '@nestjs/common/decorators';
 
 @Controller('coffees')
 export class CoffeesController {
     @Get()
-    findAll() {
-        return 'This action returns all Coffees'
+    findAll(@Res() response) {
+       response.status(200).send('This action Returns all coffees with Status 200')
     }
     @Get(':id')
     findOne(@Param('id') id:string) {
         return `This action returns #${id} Coffee`
     }
+    @HttpCode(HttpStatus.GONE)
     @Post(  )
     create(@Body() body){
         return body;
