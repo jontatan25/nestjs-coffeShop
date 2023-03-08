2b276a25df17923336bed7b734db08c97a6a62e6
Jonathan Sarmiento <jhonn.photo@gmail.com>
2023-03-08 11:53:46 -0500

    feat/Implemented Patch and delete controllers routes for coffees

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,19 +1,26 @@
-import { Controller, HttpStatus } from '@nestjs/common';
-import { Body, Get, HttpCode, Param, Post, Res } from '@nestjs/common/decorators';
+import { Controller } from '@nestjs/common';
+import { Body, Delete, Get, Param, Patch, Post } from '@nestjs/common/decorators';
 
 @Controller('coffees')
 export class CoffeesController {
     @Get()
-    findAll(@Res() response) {
-       response.status(200).send('This action Returns all coffees with Status 200')
+    findAll() {
+        return 'This action returns all Coffees'
     }
     @Get(':id')
     findOne(@Param('id') id:string) {
         return `This action returns #${id} Coffee`
     }
-    @HttpCode(HttpStatus.GONE)
-    @Post(  )
+    @Post()
     create(@Body() body){
         return body;
+    }
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() body){
+        return `This action updates #${id} Coffee`;
+    }
+    @Delete(':id')
+    remove(@Param('id') id: string){
+        return `This action removes #${id} Coffee`;
     }
 }
